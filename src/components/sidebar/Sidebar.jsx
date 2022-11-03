8303f5c789d009aa8b02540af83f4f5f08acb19f
Kate Shadrina <eolshadrina@mail.ru>
2022-11-03 22:37:37 +0300

    feature: all

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import GroupIcon from "@mui/icons-material/Group";
+import CategoryIcon from "@mui/icons-material/Category";
+import DvrIcon from "@mui/icons-material/Dvr";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SettingsSystemDaydreamIcon from "@mui/icons-material/SettingsSystemDaydream";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import LogoutIcon from "@mui/icons-material/Logout";
+//for making links to web pages
+import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
+
+function Sidebar() {
+  const { dispatch } = useContext(DarkModeContext);
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Nbirdie</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <li>
+              <DashboardIcon className="icon" />
+              <span>Dashboard</span>
+            </li>
+          </Link>
+
+          <p className="title">LISTS</p>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <GroupIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <CategoryIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
+          <li>
+            <DvrIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <QueryStatsIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountBoxIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "DARK" })}
+        ></div>
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        ></div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
